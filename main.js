71956441e723425064991c4c21af59f18a7b4d96
felgal <rag.felipe@gmail.com>
2018-11-01 09:21:42 -0300

    Melhorias nos Screeps e super screeps.

--- main.js
+++ main.js
@@ -22,6 +22,16 @@ function criarRuas(){
                 sp.room.createConstructionSite(blocos[i].x,blocos[i].y, STRUCTURE_ROAD);
             }
         }
+        
+        var sources = Game.rooms.W27N31.controller;
+        var blocos = sp.pos.findPathTo(sources.pos);
+        console.log(sources.pos);
+        for (var i = 0; i < blocos.length-1; i++) 
+            {
+                console.log(blocos[i]);
+                sp.room.createConstructionSite(blocos[i].x,blocos[i].y, STRUCTURE_ROAD);
+            }
+        
     }
 }
 
@@ -30,14 +40,16 @@ function inicializarSpawn(spawnName){
         var sp = Game.spawns[spName];
         var posX  = sp.pos.x;
         var posY = sp.pos.y;
+        
         Game.rooms[sp.room.name].createConstructionSite(posX-1, posY-1, STRUCTURE_EXTENSION);
         Game.rooms[sp.room.name].createConstructionSite(posX-1, posY, STRUCTURE_EXTENSION);
-        Game.rooms[sp.room.name].createConstructionSite(posX, posY-1, STRUCTURE_EXTENSION);
-        Game.rooms[sp.room.name].createConstructionSite(posX+1, posY-1, STRUCTURE_EXTENSION);
-        Game.rooms[sp.room.name].createConstructionSite(posX+1, posY, STRUCTURE_EXTENSION);
-        Game.rooms[sp.room.name].createConstructionSite(posX, posY-1, STRUCTURE_EXTENSION);
-        Game.rooms[sp.room.name].createConstructionSite(posX, posY-1, STRUCTURE_EXTENSION);
-        Game.rooms[sp.room.name].createConstructionSite(posX+2, posY-2, STRUCTURE_STORAGE);
+        Game.rooms[sp.room.name].createConstructionSite(posX-1, posY+1, STRUCTURE_EXTENSION);
+        
+        
+        Game.rooms[sp.room.name].createConstructionSite(posX-2, posY-1, STRUCTURE_EXTENSION);
+        Game.rooms[sp.room.name].createConstructionSite(posX-2, posY, STRUCTURE_EXTENSION);
+        Game.rooms[sp.room.name].createConstructionSite(posX-2, posY-1, STRUCTURE_EXTENSION);
+        
     }
 }
 
@@ -57,7 +69,8 @@ module.exports.loop = function () {
             console.log('Clearing non-existing creep memory:', name);
         }
     }
-
+	
+	var nomeSpawn = "Spawn";
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
@@ -65,28 +78,40 @@ module.exports.loop = function () {
     console.log('Harvesters: ' + harvesters.length);
     console.log('Builders: ' +builders.length);
     
-    if(harvesters.length < 5 && bodyCost([WORK,CARRY,MOVE])<=Game.spawns["Com"].energy) {
+    if(harvesters.length < 5 && bodyCost([WORK,CARRY,MOVE])<=Game.spawns[nomeSpawn].energy) {
         var newName = 'Harvester' + Game.time;
         console.log('Spawning new harvester: ' + newName);
-        Game.spawns['Com'].spawnCreep([WORK,CARRY,MOVE], newName, {memory: {role: 'harvester'}});
+        var sources = Game.rooms.W27N31;
+        if(sources.energyAvailable>=bodyCost([WORK,WORK,CARRY,MOVE,MOVE])){
+            Game.spawns[nomeSpawn].spawnCreep([WORK,WORK,CARRY,MOVE,MOVE], newName, {memory: {role: 'harvester'}});
+        }
+        else{
+            Game.spawns[nomeSpawn].spawnCreep([WORK,CARRY,MOVE], newName, {memory: {role: 'harvester'}});
+        }
     }
-    else if(upgraders.length < 3 && bodyCost([WORK,CARRY,MOVE])<=Game.spawns["Com"].energy) {
+    else if(upgraders.length < 3 && bodyCost([WORK,CARRY,MOVE])<=Game.spawns[nomeSpawn].energy) {
         var newName = 'Upgrader' + Game.time;
         console.log('Spawning new upgrader: ' + newName);
-        Game.spawns['Com'].spawnCreep([WORK,CARRY,MOVE], newName, {memory: {role: 'upgrader'}});
+        var sources = Game.rooms.W27N31;
+        if(sources.energyAvailable>=bodyCost([WORK,CARRY,MOVE,MOVE])){
+            Game.spawns[nomeSpawn].spawnCreep([WORK,CARRY,MOVE,MOVE], newName, {memory: {role: 'upgrader'}});
+        }
+        else{
+            Game.spawns[nomeSpawn].spawnCreep([WORK,CARRY,MOVE], newName, {memory: {role: 'upgrader'}});
+        }
     }
-    else if(builders.length < 2 && bodyCost([WORK,CARRY,MOVE])<=Game.spawns["Com"].energy) {
+    else if(builders.length < 2 && bodyCost([WORK,CARRY,MOVE])<=Game.spawns[nomeSpawn].energy) {
         var newName = 'Builder' + Game.time;
         console.log('Spawning new builder: ' + newName);
-        Game.spawns['Com'].spawnCreep([WORK,CARRY,MOVE], newName, {memory: {role: 'builder'}});
+        Game.spawns[nomeSpawn].spawnCreep([WORK,CARRY,MOVE], newName, {memory: {role: 'builder'}});
     }
     
-    if(Game.spawns['Com'].spawning) { 
-        var spawningCreep = Game.creeps[Game.spawns['Com'].spawning.name];
-        Game.spawns['Com'].room.visual.text(
+    if(Game.spawns[nomeSpawn].spawning) { 
+        var spawningCreep = Game.creeps[Game.spawns[nomeSpawn].spawning.name];
+        Game.spawns[nomeSpawn].room.visual.text(
             '🛠️' + spawningCreep.memory.role,
-            Game.spawns['Com'].pos.x + 1, 
-            Game.spawns['Com'].pos.y, 
+            Game.spawns[nomeSpawn].pos.x + 1, 
+            Game.spawns[nomeSpawn].pos.y, 
             {align: 'left', opacity: 0.8});
     }
     var sourceAtual=0;
